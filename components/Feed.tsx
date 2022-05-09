97e21556768b0ed410218b418cb390f294b63e69
pronazmul <developernazmul@gmail.com>
2022-05-10 00:33:16 +0600

    Responsive Sidear and TweetBox Design Done

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -4,9 +4,9 @@ import TweetBox from './elements/TweetBox'
 
 const Feed = () => {
   return (
-    <div className="col-span-7 border-x px-5 lg:col-span-5">
+    <div className="col-span-7 border-x lg:col-span-5">
       {/* Home & Reaload Header */}
-      <div className="mt-5 flex items-center justify-between">
+      <div className="mt-5 px-5 flex items-center justify-between">
         <h1 className="text-xl font-bold">Home</h1>
         <RefreshIcon className="h-8 w-8 cursor-pointer text-twitter transition-all duration-1000 hover:rotate-180 active:scale-125" />
       </div>
@@ -14,6 +14,7 @@ const Feed = () => {
       <div>
         <TweetBox />
       </div>
+      
     </div>
   )
 }
--- components/elements/TweetBox.tsx
+++ components/elements/TweetBox.tsx
@@ -8,30 +8,38 @@ import {
 } from '@heroicons/react/outline'
 
 const TweetBox = () => {
+  const [input, setInput] = React.useState<string>('')
+
   return (
-    <div className="bg-yellow-300">
+    <div className="flex space-x-2 p-5">
       <img
-        className="h-14 w-14 rounded-full object-fill "
+        className="mt-5 h-14 w-14 rounded-full object-fill "
         src="images/avatar-men.jpg"
         alt="tweet box icon"
       />
-
-      <div>
-        <form>
+      <div className="flex flex-1 items-center pl-2">
+        <form className="flex flex-1 flex-col">
           <input
-            className="bg-transparent"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className="h-24 bg-transparent text-xl outline-none placeholder:text-xl"
             type="text"
             placeholder="What's happning?"
           />
-          <div className='flex'>
-            <div className='flex'>
-                <PhotographIcon className="h-5 w-5" />
-                <SearchCircleIcon className="h-5 w-5" />
-                <EmojiHappyIcon className="h-5 w-5" />
-                <CalendarIcon className="h-5 w-5" />
-                <LocationMarkerIcon className="h-5 w-5" />
+          <div className="flex items-center">
+            <div className="flex flex-1 space-x-2 text-twitter">
+              <PhotographIcon className="h-5 w-5 cursor-pointer transition-transform duration-200 ease-out hover:scale-150" />
+              <SearchCircleIcon className="h-5 w-5" />
+              <EmojiHappyIcon className="h-5 w-5" />
+              <CalendarIcon className="h-5 w-5" />
+              <LocationMarkerIcon className="h-5 w-5" />
             </div>
-            <button>Tweet</button>
+            <button
+              disabled={!input}
+              className="cursor-pointer rounded-full bg-twitter px-5 py-2 font-bold text-white disabled:opacity-50"
+            >
+              Tweet
+            </button>
           </div>
         </form>
       </div>
