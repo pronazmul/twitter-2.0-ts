fa9c09609757ae39d2c5a353cc7bff403a17283d
pronazmul <developernazmul@gmail.com>
2022-05-10 20:40:32 +0600

    Data Fatched Using SSR, Sanity CMS and GROQ Schema

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,12 +1,17 @@
 import { RefreshIcon } from '@heroicons/react/outline'
 import React from 'react'
 import TweetBox from './elements/TweetBox'
+import { Tweet } from './../typings.d'
 
-const Feed = () => {
+interface Props {
+  tweets: Tweet[]
+}
+
+const Feed = ({ tweets }: Props) => {
   return (
     <div className="col-span-7 border-x lg:col-span-5">
       {/* Home & Reaload Header */}
-      <div className="mt-5 px-5 flex items-center justify-between">
+      <div className="mt-5 flex items-center justify-between px-5">
         <h1 className="text-xl font-bold">Home</h1>
         <RefreshIcon className="h-8 w-8 cursor-pointer text-twitter transition-all duration-1000 hover:rotate-180 active:scale-125" />
       </div>
@@ -14,7 +19,8 @@ const Feed = () => {
       <div>
         <TweetBox />
       </div>
-      
+      {/* Tweet */}
+      <div></div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,26 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Feed from '../components/Feed'
+import { fetchTweets } from '../utilits/fetchTweets'
 import Sidebar from './../components/Sidebar'
 import Widgets from './../components/Widgets'
+import { Tweet } from './../typings.d'
 
-const Home: NextPage = () => {
+interface Props {
+  tweets: Tweet[]
+}
+
+const Home = ({ tweets }: Props) => {
   return (
-    <div className="mx-auto max-h-screen  lg:max-w-6xl overflow-hidden">
+    <div className="mx-auto max-h-screen  overflow-hidden lg:max-w-6xl">
       <Head>
         <title>Twitter 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="grid grid-cols-9">
-        {/* Sidebar */}
         <Sidebar />
-        {/* Feed  */}
-        <Feed />
-        {/* Widegts */}
+        <Feed tweets={tweets} />
         <Widgets />
       </main>
     </div>
@@ -25,3 +28,10 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const tweets = await fetchTweets()
+  return {
+    props: { tweets },
+  }
+}
